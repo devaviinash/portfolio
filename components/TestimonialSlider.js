4869a4ea83b1cfc66cfd9022f32d35438ab8d79d
Avinash Chavan <151129318+devaviinash@users.noreply.github.com>
2024-11-08 07:48:05 +0530

    Update TestimonialSlider.js

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -3,21 +3,21 @@ const testimonialSlider = [
    {
       image: "/t-avt-1.png",
       name: "Anne Smith",
-      position: "Customer",
+      position: "",
       message:
          "Her analytical acumen and innovative problem-solving make her a standout in data science. She has an exceptional ability to extract meaningful insights from complex datasets, driving impactful results.",
    },
    {
       image: "/t-avt-2.png",
       name: "Jane Doe",
-      position: "Customer",
+      position: "",
       message:
          "Her mastery of data modeling and visualization is complemented by her strategic approach to tackling challenges. Her commitment to excellence and her enthusiasm for uncovering actionable insights make her an invaluable asset to any team",
    },
    {
       image: "/t-avt-3.png",
       name: "Jhon Doe",
-      position: "Customer",
+      position: "",
       message:
          "Her passion for data science is evident in her meticulous attention to detail and her drive to deliver meaningful results. Her enthusiasm and dedication inspire everyone around her.",
    },
